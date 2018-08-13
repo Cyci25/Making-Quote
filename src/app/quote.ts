ca38aef56714c28f151525c3b7f95d339c10462d
Cyci25 <c2muriithi@gmail.com>
2018-08-13 10:05:37 +0300

    Made changes on the quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,3 @@
-// export class Quote {
-//     constructor (public author:string, public quote:string, public submiter:string, public timeDate:Date){}
-// }
 export class Quote {
-    public showDescription:boolean
-    constructor(public id:number, public name:string,public description:string,public completeDate:Date){
-        this.showDescription=true
-
-    }
+    constructor (public author:string, public quote:string, public submiter:string, public timeDate:Date){}
 }
