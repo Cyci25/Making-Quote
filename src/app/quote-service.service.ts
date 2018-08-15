ecbbe89bee8710a38bd00458b96dd565ecffb750
Cyci25 <c2muriithi@gmail.com>
2018-08-15 17:32:58 +0300

    created a quote service service

---
+++ src/app/quote-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {Quotes} from './quotes';
+import {Quote} from './quote';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuoteServiceService {
+
+  constructor() { }
+
+  getAllQuotes():Quote[]{
+    return Quotes;
+  }
+
+}
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,10 @@
-export class Quotes {
-}
+import {Quote} from './quote';
+
+export let Quotes: Quote[] =
+    [
+        new Quote('Cynthia Joel', 'Sometimes Cows do Fly', 'Cynthia', new Date(2018, 6, 20)),
+        new Quote('Cynthia Joel', 'Sometimes Cows do Fly', 'Cynthia', new Date(2018, 6, 20)),
+        new Quote('Cynthia Joel', 'Sometimes Cows do Fly', 'Cynthia', new Date(2018, 6, 20))
+    ]
+
+
