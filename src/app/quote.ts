db629b65be125596e661a993b9ee41ff5fff27bf
Cyci25 <c2muriithi@gmail.com>
2018-08-23 16:38:09 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,9 +0,0 @@
-export class Quote {
-    constructor (
-        public index:number,
-        public author:string,
-        public quote:string,
-        public submiter:string,
-        public timeDate:Date
-    ){}
-}
